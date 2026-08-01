21d66b55a0ff5532bb81924825ca092ec4c24878
Ayush Singh <singhayush3547@gmail.com>
2026-08-01 20:30:20 +0530

    fixed partial deployment bug

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Base route for dev
-if (process.env.NODE_ENV !== "production") {
-    app.get("/", (req, res) => {
-        res.send("Runner's Arc Backend Running"); 
-    });
-}
+// Base route (health check)
+app.get("/", (req, res) => {
+    res.send("Runner's Arc Backend Running"); 
+});
 
 app.use("/api/v2/auth", authRoutes);
 app.use("/api/v2/runs", runRoutes);
@@ -28,14 +26,6 @@ app.use("/api/v2/stats", statsRoutes);
 app.use("/api/v2/territories", territoryRoutes);
 app.use("/api/v2/clans", clanRoutes);
 
-const path = require("path");
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-    app.get(/(.*)/, (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
-    });
-}
 
 const PORT = process.env.PORT || 5000;
 
