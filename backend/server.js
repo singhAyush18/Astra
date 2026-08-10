e5a425d7cfa3390fdcea9f79fadac269db0cf567
Ayush Singh <singhayush3547@gmail.com>
2026-08-11 00:17:27 +0530

    fix: update CORS for production

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:5173',
-        'http://localhost:5174'
-    ],
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        process.env.FRONTEND_URL
+    ].filter(Boolean),
     credentials: true
 }));
 
